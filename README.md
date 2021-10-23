72810b80b005ffbd798ff682be380ec501378bbc
Indra Palijama <indrapalijama@gmail.com>
2021-10-24 01:06:28 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+<br />
+<p align="center">
+  <h3 align="center">NPX Card</h3>
+  <p align="center">
+    My Personal Business Card 
+    <br />
+  </p>
+</p>
+
+
+
+
+### Running
+
+```bash
+npx indrapalijama
+```
+
---
+++ card.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const boxen = require('boxen');
+const chalk = require('chalk');
+const clear = require('clear');
+
+
+clear();
+
+const lines = [
+  `${chalk.white.bold('Samudra Palijama')} / ${chalk.hex('#0094C6')('@indrapalijama')}`,
+  `${chalk.hex('#d4d4d4')('Mobile Engineer @ Computrade Technology International')}`,
+  '',
+  `${chalk.white.bold('Web')}        ${chalk.hex('#0094C6').underline('https://indrapalijama.github.io')}`,
+  `${chalk.white.bold('GitHub')}     ${chalk.hex('#0094C6').underline('https://github.com/indrapalijama')}`,
+  `${chalk.white.bold('LinkedIn')}   ${chalk.hex('#0094C6').underline('https://linkedin.com/in/samudrapalijama/')}`,
+  `${chalk.white.bold('Mail')}       ${chalk.hex('#0094C6').underline('mailto:indrapalijama@gmail.com')}`
+];
+
+const output = boxen(lines.join('\n'), {
+  backgroundColor: '#141414',
+  borderColor: '#d4d4d4',
+  borderStyle: 'single',
+  margin: 1,
+  padding: 1
+});
+
+console.log(output);
